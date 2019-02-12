d8439a90dc0824dca7514b10af8a57036c65b51b
KiranMayee Maddi <KiranMayeeMaddi@users.noreply.github.com>
2019-02-12 01:29:46 -0500

    Add files via upload

---
+++ twitterStream.py
@@ -0,0 +1,170 @@
+
+from pyspark import SparkConf, SparkContext
+from pyspark.streaming import StreamingContext
+from pyspark.streaming.kafka import KafkaUtils
+import operator
+import numpy as np
+import matplotlib
+matplotlib.use("agg")
+import matplotlib.pyplot as plt
+
+
+def main():
+    conf = SparkConf().setMaster("local[2]").setAppName("Streamer")
+    sc = SparkContext(conf=conf)
+    sc.setLogLevel("WARN") 
+    ssc = StreamingContext(sc, 10)   # Create a streaming context with batch interval of 10 sec
+    ssc.checkpoint("checkpoint")
+
+    pwords = load_wordlist("positive.txt")
+    nwords = load_wordlist("negative.txt")
+    counts = stream(ssc, pwords, nwords, 100)
+    make_plot(counts)
+
+
+def make_plot(counts):
+    """
+    Plot the counts for the positive and negative words for each timestep.
+    Use plt.show() so that the plot will popup.
+    """
+    # YOUR CODE HERE
+    positive = []
+    negative = []
+    [positive.append(word[1]) if word[0] == "positive" else negative.append(word[1]) for count in counts for word in count]
+    fig = plt.figure()
+    time = list(range(len(positive)))
+    plt.xticks(np.arange(min(time), max(time)+1, 1.0))
+    plt.plot(time, positive, "bo-", label = "Positive")
+    plt.plot(time, negative, "go-", label = "Negative")
+    plt.xlabel("Time step")
+    plt.ylabel("Word count")
+    plt.legend(loc = "upper left")
+    fig.savefig("plot.png")
+    plt.show()
+    
+def load_wordlist(filename):
+    """ 
+    This function should return a list or set of words from the given filename.
+    """
+    # YOUR CODE HERE
+    try:
+        with open(filename) as file:
+            words = file.read().split("\n")
+    except IOError as e:
+        print(e)
+    return set(words)
+
+def updateFunction(newValues, runningCount):
+        if runningCount is None:
+            runningCount = 0
+        return sum(newValues, runningCount)
+    
+
+def stream(ssc, pwords, nwords, duration):
+    kstream = KafkaUtils.createDirectStream(
+        ssc, topics = ['twitterstream'], kafkaParams = {"metadata.broker.list": 'localhost:9092'})
+    tweets = kstream.map(lambda x: x[1])
+    # Each element of tweets will be the text of a tweet.
+    # You need to find the count of all the positive and negative words in these tweets.
+    # Keep track of a running total counts and print this at every time step (use the pprint function).
+    # YOUR CODE HERE
+
+    tweets = tweets.flatMap(lambda x: x.split(" "))
+    posNeg = tweets.filter(lambda word: (word in  pwords) or (word in nwords))
+    posNegUnion = posNeg.map(lambda x: ("positive", 1) if x in pwords else ("positive", 0)).union(tweets.map(lambda x: ("negative", 1) if x in nwords else ("negative", 0)))
+    posNegCount = posNegUnion.reduceByKey(lambda x,y: x+y)  
+    
+    runningCounts = posNegCount.updateStateByKey(updateFunction)
+    runningCounts.pprint()
+   
+    # Let the counts variable hold the word counts for all time steps
+    # You will need to use the foreachRDD function.
+    # For our implementation, counts looked like:
+    #   [[("positive", 100), ("negative", 50)], [("positive", 80), ("negative", 60)], ...]
+    counts = []
+    # YOURDSTREAMOBJECT.foreachRDD(lambda t,rdd: counts.append(rdd.collect()))
+    posNegCount.foreachRDD(lambda t,rdd: counts.append(rdd.collect()))
+    ssc.start() # Start the computation
+    ssc.awaitTerminationOrTimeout(duration)
+    ssc.stop(stopGraceFully=True)
+    return counts
+
+
+if __name__=="__main__":
+    main()
+
+'''
+Sample Output
+-------------------------------------------
+Time: 2019-02-08 04:26:00
+-------------------------------------------
+('positive', 62580)
+('negative', 59321)
+
+-------------------------------------------
+Time: 2019-02-08 04:26:10
+-------------------------------------------
+('positive', 124069)
+('negative', 117288)
+
+-------------------------------------------
+Time: 2019-02-08 04:26:20
+-------------------------------------------
+('positive', 188202)
+('negative', 177081)
+
+-------------------------------------------
+Time: 2019-02-08 04:26:30
+-------------------------------------------
+('positive', 251770)
+('negative', 236241)
+
+-------------------------------------------
+Time: 2019-02-08 04:26:40
+-------------------------------------------
+('positive', 309520)
+('negative', 282489)
+
+-------------------------------------------
+Time: 2019-02-08 04:26:50
+-------------------------------------------
+('positive', 380769)
+('negative', 322865)
+
+-------------------------------------------
+Time: 2019-02-08 04:27:00
+-------------------------------------------
+('positive', 459225)
+('negative', 367210)
+
+-------------------------------------------
+Time: 2019-02-08 04:27:10
+-------------------------------------------
+('positive', 533400)
+('negative', 409534)
+
+-------------------------------------------
+Time: 2019-02-08 04:27:20
+-------------------------------------------
+('positive', 607440)
+('negative', 451698)
+
+-------------------------------------------
+Time: 2019-02-08 04:27:30
+-------------------------------------------
+('positive', 690588)
+('negative', 499158)
+
+-------------------------------------------
+Time: 2019-02-08 04:27:40
+-------------------------------------------
+('positive', 770958)
+('negative', 545461)
+
+-------------------------------------------
+Time: 2019-02-08 04:27:50
+-------------------------------------------
+('positive', 854621)
+('negative', 594221)
+'''
+
